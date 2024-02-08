76b36f4b7c05b5f8426930e1d32de382de2450d7
van <erivanbuenaventura@gmail.com>
2024-02-08 21:13:58 +0800

    updated scripts using podman instead of docker

--- 02/demos/Demos/install-test-database.ps1
+++ 02/demos/Demos/install-test-database.ps1
@@ -2,11 +2,14 @@
 
 #docker pull postgres
 podman pull docker.io/library/postgres
-docker run --name postgres_test -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
-docker container rm --force postgres_test
+#docker run --name postgres_test -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
+podman run --name postgres_test -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
+#docker container rm --force postgres_test
+podman container rm --force postgres_test
 
 # Create a Docker volume for persistent storage and rebuild the container
-docker volume create pgdata
+#docker volume create pgdata
+podman volume create pgdata
 #docker run --name postgres_test -v pgdata:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
 podman run --name postgres_test -v pgdata:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
 
